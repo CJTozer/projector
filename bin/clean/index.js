77e403e2f5d97d908fa15b865b1c4915d7d6e567
Chris Tozer <christopherjtozer@gmail.com>
2017-01-17 22:53:40 +0000

    Better clean script

--- bin/clean/index.js
+++ bin/clean/index.js
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
-const fs = require( 'fs' );
-const path = require( 'path' );
+const fs = require('fs-extra');
+const path = require('path');
 
 const CSS_DIR = path.join( 'static', 'css' );
 
-try {
-  fs.rmdirSync( CSS_DIR );
-} catch ( e ) {
-  // Ignore error - assume directory doesn't exist...
-}
-fs.mkdirSync( CSS_DIR );
+// Ensure the static CSS directory is present and clean.
+fs.ensureDirSync(CSS_DIR);
+fs.emptydirSync(CSS_DIR);
