c2c56c42d84db118515276bbb7693b8c3f6ce5a5
Chris Tozer <christopherjtozer@gmail.com>
2017-01-21 16:04:05 +0000

    Split Gantt out into its own template

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 const express = require( 'express' );
 const logger = require( 'morgan' );
-const template = require( 'jade' ).compileFile( `${__dirname}/source/templates/homepage.jade` );
+const jade = require( 'jade' );
+const fs = require( 'fs' );
 const config = require( './lib/config' );
 
 const app = express();
+const homepage = jade.compileFile( `${__dirname}/source/templates/homepage.jade` );
+const ganttpage = jade.compileFile( `${__dirname}/source/templates/gantt.jade` );
 
 app.use( logger( 'dev' ) );
 app.use( express.static( `${__dirname}/static` ) );
@@ -12,9 +15,14 @@ app.use( '/scripts', express.static( `${__dirname}/source/scripts` ) );
 
 app.get( '/', ( req, res, next ) => {
   try {
-    const html = template( {
+    // Get projects
+    const files = fs.readdirSync( './data' )
+      .filter( f => f.endsWith( '.yaml' ) )
+      .map( f => f.slice( 0, -5 ) );
+
+    const html = homepage( {
       title: 'Home',
-      task_array: JSON.stringify( config.get_tasks( 'example.yaml' ) ),
+      files,
     } );
     res.send( html );
   } catch ( e ) {
@@ -22,10 +30,24 @@ app.get( '/', ( req, res, next ) => {
   }
 } );
 
-app.get( '/data', ( req, res, next ) => {
+app.get( '/gantt', ( req, res, next ) => {
+  // @@@ Get the correct data based on request parameter
   try {
-    const json = config.get_tasks( 'example.yaml' );
-    res.send( json );
+    // Get projects
+    // @@@ Don't duplicate this?
+    const files = fs.readdirSync( './data' )
+      .filter( f => f.endsWith( '.yaml' ) )
+      .map( f => f.slice( 0, -5 ) );
+
+    const project = req.query.project || 'example';
+    // @@@ If project not in files, show not found page.
+
+    const html = ganttpage( {
+      title: 'Home',
+      files,
+      task_array: JSON.stringify( config.get_tasks( `${project}.yaml` ) ),
+    } );
+    res.send( html );
   } catch ( e ) {
     next( e );
   }
